c18bab06312572eb1f85c9ae625cc056e162bb6a
lengge <1052730250@qq.com>
2017-08-15 17:22:22 +0800

    Initial Commit

---
+++ WexSingleChooseView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WexSingleChooseView
+//
+//  Created by wex on 2017/8/15.
+//  Copyright © 2017年 wex. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
